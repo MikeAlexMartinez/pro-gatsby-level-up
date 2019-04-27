a6b33154c990dfb7db5a5b9159c7b1d7dedd3dcd
Michael Alexander Martinez <mike.alex.martinez@gmail.com>
2019-04-27 11:27:02 +0100

    installed gatsby netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,5 +48,6 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`, // Needs to be last in array
   ],
 };
